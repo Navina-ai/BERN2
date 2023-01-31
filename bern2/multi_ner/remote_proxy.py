25b91130b07914fe25473b92a45c61ec52403b31
Zach Avraham <zach@navina.ai>
2023-01-31 15:47:24 +0200

    Small fixes.

--- bern2/multi_ner/remote_proxy.py
+++ bern2/multi_ner/remote_proxy.py
@@ -4,10 +4,12 @@ import os
 import boto3
 import numpy as np
 
+
 class TritonModelProxy:
     def __init__(self, model_name, batch_size=32):
-        self.model_name=model_name
-        self.batch_size=batch_size
+        self.model_name = model_name
+        self.batch_size = batch_size
+
     def __call__(self, *args, **kwargs):
         runtime_sm_client = boto3.client(service_name="sagemaker-runtime",
                                          aws_access_key_id=os.environ['AwsAccessKey'],
@@ -18,14 +20,14 @@ class TritonModelProxy:
         res = list()
 
         while i < len(input_ids):
-            ids_ = input_ids[i:i + batch_size]
-            mask_ = attention_mask[i:i + batch_size]
-            i += batch_size
+            ids_ = input_ids[i:i + self.batch_size]
+            mask_ = attention_mask[i:i + self.batch_size]
+            i += self.batch_size
             payload = {
                 "inputs": [{'name': 'input_ids', 'shape': ids_.shape, "datatype": "INT64", "data": ids_.tolist()},
                            {'name': 'attention_mask', 'shape': mask_.shape, "datatype": "INT64",
                             "data": mask_.tolist()}]}
-            endpoint_response = runtime_sm_client.invoke_endpoint(EndpointName=model_name,
+            endpoint_response = runtime_sm_client.invoke_endpoint(EndpointName=self.model_name,
                                                                   ContentType='application/octet-stream',
                                                                   Body=json.dumps(payload))
             parsed_response = json.loads(endpoint_response["Body"].read().decode("utf8"))['outputs'][0]
@@ -33,7 +35,9 @@ class TritonModelProxy:
             res.append(parsed_data)
 
         return np.concatenate(res)
+
     def to(self, *args, **kwargs):
         return self
+
     def eval(self, *args, **kwargs):
         pass
