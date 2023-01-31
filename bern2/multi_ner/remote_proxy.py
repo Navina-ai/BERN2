e4eb7bec554a7c0ee8b7dd04ce506039634d697f
Zach Avraham <zach@navina.ai>
2023-01-31 14:07:54 +0200

    Indentation.

--- bern2/multi_ner/remote_proxy.py
+++ bern2/multi_ner/remote_proxy.py
@@ -7,7 +7,9 @@ import numpy as np
 
 def create_remote_inference_proxy(model_name='bern2', batch_size=4):
     def call_remote_inference(**inputs):
-        runtime_sm_client = boto3.client(service_name="sagemaker-runtime", aws_access_key_id=os.environ['AwsAccessKeyId'], aws_secret_access_key=os.environ['AwsSecretAccessKey'])
+        runtime_sm_client = boto3.client(service_name="sagemaker-runtime",
+                                         aws_access_key_id=os.environ['AwsAccessKeyId'],
+                                         aws_secret_access_key=os.environ['AwsSecretAccessKey'])
         input_ids = inputs['input_ids']
         attention_mask = inputs['attention_mask']
         i = 0
@@ -17,9 +19,13 @@ def create_remote_inference_proxy(model_name='bern2', batch_size=4):
             ids_ = input_ids[i:i + batch_size]
             mask_ = attention_mask[i:i + batch_size]
             i += batch_size
-            payload = {"inputs": [{'name': 'input_ids', 'shape': ids_.shape, "datatype": "INT64", "data": ids_.tolist()},
-                                  {'name': 'attention_mask', 'shape': mask_.shape, "datatype": "INT64", "data": mask_.tolist()}]}
-            endpoint_response = runtime_sm_client.invoke_endpoint(EndpointName=model_name, ContentType='application/octet-stream', Body=json.dumps(payload))
+            payload = {
+                "inputs": [{'name': 'input_ids', 'shape': ids_.shape, "datatype": "INT64", "data": ids_.tolist()},
+                           {'name': 'attention_mask', 'shape': mask_.shape, "datatype": "INT64",
+                            "data": mask_.tolist()}]}
+            endpoint_response = runtime_sm_client.invoke_endpoint(EndpointName=model_name,
+                                                                  ContentType='application/octet-stream',
+                                                                  Body=json.dumps(payload))
             parsed_response = json.loads(endpoint_response["Body"].read().decode("utf8"))['outputs'][0]
             parsed_data = np.array(parsed_response['data']).reshape(parsed_response['shape'])
             res.append(parsed_data)
