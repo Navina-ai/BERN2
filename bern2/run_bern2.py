08f13bb1c4ab52383344edca591bb4ffb2994053
Ben Laor <ben.laor@navina.ai>
2023-02-22 16:06:48 +0200

    now we dont use it

--- bern2/run_bern2.py
+++ bern2/run_bern2.py
@@ -351,27 +351,17 @@ class LocalBERN2():
     def ner(self, pubtator_file, output_mtner, base_name) -> dict:
         # Run neural model
         start_time = time.time()
-        argparser = argparse.ArgumentParser()
-        argparser.add_argument('--seed', type=int, help='random seed for initialization', default=1)
-        argparser.add_argument('--max_seq_length', type=int,
-                               help='The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.',
-                               default=128)
-        argparser.add_argument('--mtner_home', help='biomedical language model home')
-        argparser.add_argument('--disease_only', help='use disease only NER', type=bool, default=True)
-        argparser.add_argument('--time_format', help='time format', default='[%d/%b/%Y %H:%M:%S.%f]')
-        argparser.add_argument("--ner_model_name_or_path", type=str, default="dmis-lab/bern2-ner")
-        argparser.add_argument("--load_model_manually", action="store_true")
-        argparser.add_argument("--s3_bucket", type=str, default="data-science-repository")
-        argparser.add_argument("--local_output", type=str, default="local_output")
-        argparser.add_argument("--use_remote_proxy", action="store_true")
-        argparser.add_argument('--batch_size', type=int, help='The batch size to be run.', default=4)
-        argparser.add_argument("--download_model_bin_file", action="store_true")
-        mt_ner_params = argparser.parse_args()
-        mt_ner_params.model_name_or_path = self.ner_model_name_or_path
-        mt_ner_params.load_model_manually = self.load_model_manually
-        mt_ner_params.use_remote_proxy = self.use_remote_proxy
-        mt_ner_params.batch_size = self.batch_size
-        mt_ner_params.download_model_bin_file = self.download_model_bin_file
+        mt_ner_params = argparse.Namespace(seed=1,
+                                           max_seq_length=128,
+                                           mtner_home=None,
+                                           disease_only=True,
+                                           time_format="dmis-lab/bern2-ner",
+                                           ner_model_name_or_path=self.ner_model_name_or_path or 'dmis-lab/bern2-ner',
+                                           load_model_manually=self.load_model_manually,
+                                           s3_bucket='data-science-repository',
+                                           local_output='local_output',
+                                           use_remote_proxy=self.use_remote_proxy,
+                                           batch_size=self.batch_size, download_model_bin_file=self.download_model_bin_file)
 
         mt_ner_model = MTNER(mt_ner_params)
         base_name = pubtator_file.split('.')[0]
@@ -434,8 +424,8 @@ def initialize_bern2_annotator(max_word_len: int = 50,
                                mtner_home: str = os.path.join(os.path.expanduser('~'), 'bern', 'mtnerHome'),
                                use_neural_normalizer: bool = False, keep_files: bool = False,
                                ner_model_name_or_path: str = 'dmis-lab/bern2-ner', load_model_manually: bool = False,
-                               download_model_bin_file: bool = True, s3_bucket: str = 'data-science-repository',
-                               local_output: str = 'local_output', use_remote_proxy: bool = False, batch_size: int = 4,
+                               s3_bucket: str = 'data-science-repository', local_output: str = 'local_output',
+                               use_remote_proxy: bool = False, batch_size: int = 4,
                                seed: int = 2019, time_format: str = '[%d/%b/%Y %H:%M:%S.%f]'):
     # initialize bern2
     if initialize_bern2_annotator.annotator is None:
@@ -447,7 +437,6 @@ def initialize_bern2_annotator(max_word_len: int = 50,
                                                           keep_files=keep_files,
                                                           ner_model_name_or_path=ner_model_name_or_path,
                                                           load_model_manually=load_model_manually,
-                                                          download_model_bin_file=download_model_bin_file,
                                                           s3_bucket=s3_bucket,
                                                           local_output=local_output,
                                                           use_remote_proxy=use_remote_proxy,
